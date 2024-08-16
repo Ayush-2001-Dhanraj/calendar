6f91cfc3f3af7c47f7eadfba02097013ead8af8d
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-08-16 22:00:19 +0530

    Border styles

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import styles from "./footer.module.css";
 
 function Footer() {
-  return <div className={styles.footer}>Made with ❤ by Ayush Dhanraj</div>;
+  return (
+    <div className={styles.footer}>
+      <a
+        href="https://github.com/Ayush-2001-Dhanraj"
+        className={styles.creatorName}
+      >
+        Ayush Dhanraj
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
